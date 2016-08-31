a21806a234a2d8c1c0bf498db18b9bb5e4edf7c6
SBrownSPS <tshbby326@yahoo.com>
2016-08-30 21:53:58 -0400

    First commit for Assignment 1.

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -4,24 +4,42 @@
 
 
 def listDivide( numbers, divide = 2 ):
-"""numbers is a list""" 
-"""divide is the divisor"""
-"""returns the number of list items that are divisble by the divisor"""
-int divisibles
-for anumber in numbers:
-if anumber % divide == 0:
-divisibles += 1 
-return divisibles
+    """tests divisibility
+    :param numbers: numbers is a list
+    :param divide: divide is the divisor
+    :return: returns the number of list items that are divisble by the divisor
+    """
+    divisibles = 0
+    for anumber in numbers:
+        if anumber % divide == 0:
+           divisibles += 1
+    return divisibles
 
-"""class ListDivideException"""
+class ListDivideException(Exception):
+    pass
 
 def testListDivide( ):
-"""raise exception if any fail"""
-listDivide([1,2,3,4,5])
-listDivide([2,4,6,8,10])
-listDivide([30, 54, 63,98, 100], divide=10)
-listDivide([])
-listDivide([1,2,3,4,5], 1)
-return
+    """raises exception if any fail"""
+    try:
+        listDivide([1,2,3,4,5])
+    except:
+        raise ListDivideException('Unknown Issue')
+    try:
+        listDivide([2,4,6,8,10])
+    except:
+        raise ListDivideException('Unknown Issue')
+    try:
+        listDivide([30,54,63,98,100],divide=10)
+    except:
+        raise ListDivideException('Unknown Issue')
+    try:
+        listDivide([])
+    except:
+        raise ListDivideException('Unknown Issue')
+    try:
+        listDivide([1,2,3,4,5],1)
+    except:
+        raise ListDivideException('Unknown Issue')
+    return
 
-testListDivide
+testListDivide()
